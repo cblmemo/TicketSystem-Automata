7c303e63e234ee91c5272d753d42286c6956c5bc
Rainy-Memory <2630737606@qq.com>
2021-05-14 11:14:02 +0800

    test BPlusTree M && L

--- BackEnd/include/BPlusTree.h
+++ BackEnd/include/BPlusTree.h
@@ -19,7 +19,7 @@ using RainyMemory::LRUCacheMemoryPool;
 //#define debug
 
 namespace RainyMemory {
-    template<class key, class data, int M = 50, int L = 200>
+    template<class key, class data, int M = 200, int L = 50>
     class BPlusTree {
         /*
          * class BPlusTree
