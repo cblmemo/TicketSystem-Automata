41968a318487cb9b9f20a82ec491a04be6d96a6c
Rainy-Memory <2630737606@qq.com>
2021-05-28 16:20:12 +0800

    Adjusting arguments...

--- BackEnd/src/Global.h
+++ BackEnd/src/Global.h
@@ -35,10 +35,10 @@ enum coefficient {
     SEAT_NUM_INFINITY = 2000000000,
     USER_CACHE_SIZE = 500,
     TRAIN_CACHE_SIZE = 1000,
-    BPLUSTREE_L = 50,
-    BPLUSTREE_M = 50,
-    MULTI_BPLUSTREE_L = 50,
-    MULTI_BPLUSTREE_M = 50
+    BPLUSTREE_L = 100,
+    BPLUSTREE_M = 100,
+    MULTI_BPLUSTREE_L = 100,
+    MULTI_BPLUSTREE_M = 100
 };
 
 constexpr int lengthOfChineseCharacters(int length) {
