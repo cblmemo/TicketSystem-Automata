4ed9b682d0930c66e02a981e42d2df97b5ed51b7
Rainy-Memory <2630737606@qq.com>
2021-05-30 13:54:11 +0800

    Some small update.

--- BackEnd/src/Global.h
+++ BackEnd/src/Global.h
@@ -49,9 +49,9 @@ template<int len>
 struct hash_string_t {
     hash_t operator()(const string_t<len> &o) const {
         int l = o.length();
-        int h1 = l, h2 = l;
-        for (int i = 0; i < l; i++)h1 = (h1 << 7) ^ (h1 >> 25) ^ o[i], h2 = (h2 << 11) ^ (h2 >> 21) ^ o[i];
-        return (hash_t(h1) << 32) | hash_t(h2);
+        hash_t res = 0;
+        for (int i = 0; i < l; i++)res = res * 9973 + o[i];
+        return res;
     }
 };
 
