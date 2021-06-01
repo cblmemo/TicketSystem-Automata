a7301917a43ff0b4f510c29cb3bebb145dda5aa4
Rainy-Memory <2630737606@qq.com>
2021-06-01 17:09:32 +0800

    update FrontEnd version.

--- FrontEnd/BackEnd/src/Administrator.cpp
+++ FrontEnd/BackEnd/src/Administrator.cpp
@@ -3,14 +3,6 @@
 //
 
 #include "Administrator.h"
-#include <thread> // std::this_thread
-
-void Administrator::initialize() {
-    Ptilopsis = new Parser;
-    Saria = new UserManager(UserIndexPath, UserStoragePath);
-    Silence = new TrainManager(TrainIndexPath, TrainStoragePath, TrainStationPath);
-    Ifrit = new OrderManager(Saria, Silence, OrderIndexPath, OrderPendingPath);
-}
 
 void Administrator::clean() {
     Saria->clear();
@@ -18,7 +10,12 @@ void Administrator::clean() {
     Ifrit->clear();
 }
 
-Administrator::Administrator() = default;
+Administrator::Administrator() {
+    Ptilopsis = new Parser;
+    Saria = new UserManager(UserIndexPath, UserStoragePath);
+    Silence = new TrainManager(TrainIndexPath, TrainStoragePath, TrainStationPath);
+    Ifrit = new OrderManager(Saria, Silence, OrderIndexPath, OrderPendingPath);
+}
 
 Administrator::~Administrator() {
     delete Ptilopsis;
@@ -68,18 +65,60 @@ std::string Administrator::process(const std::string &cmd) {
     }
 }
 
-int Administrator::runProgramme() {
-    initialize();
+void Administrator::runProgramme() {
+    int sListen = socket(AF_INET, SOCK_STREAM, 0);
+    sockaddr_in sin;
+    sin.sin_family = AF_INET;
+    sin.sin_port = htons(10240);
+    sin.sin_addr.s_addr = htonl(INADDR_ANY);
+    if (bind(sListen, (struct sockaddr *) &sin, sizeof(sin)) == -1) {
+        printf("Socket:  \033[;31mBind Error!\033[0m");
+        return;
+    }
+    if (listen(sListen, 5) == -1) {
+        printf("Socket:  \033[;31mListen Error!\033[0m");
+        return;
+    }
+    std::cout << "Socket:  \033[;32mSocket Server Initialization Finished.\033[0m" << std::endl;
+    
+    int sClient;    // 创建客户端socket
+    sockaddr_in clientAdd;  // 客户端的地址信息
+    socklen_t clientAddLen = sizeof(clientAdd);
+    char receiveDat[10240]; // 接收数据缓冲区
+    char command[10240], sendResult[10240];
+    
     string cmd;
-//    std::cout << "BackEnd: \033[32mBackEnd Initialization Finished.\033[0m" << std::endl;
+    std::cout << "BackEnd: \033[32mBackEnd Initialization Finished.\033[0m" << std::endl;
     bool flag = true;
     while (flag) {
-        getline(std::cin, cmd);
-//        std::cout << "BackEnd: \033[;36m<< Handling Operation: \033[35m\"" << cmd << "\"\033[0m" << std::endl;
-        std::string result = process(cmd);
-        if (result == "bye\n")flag = false;
-        if (result == "[Error]Invalid command.\n")return 1;
-        std::cout << result;
+        memset(command, 0, sizeof(command));
+        std::cout << "Socket:  \033[;33m<< Begin Listening...\033[0m" << std::endl;
+        sClient = accept(sListen, (struct sockaddr *) &clientAdd, &clientAddLen);
+        if (sClient == -1) {
+            printf("Socket:  \033[;31mAccept Error!\033[0m");
+            continue;
+        }
+        recv(sClient, receiveDat, 10240, 0);
+        std::cout << "Socket:  \033[;33m>> Receive Data: \033[35m\"" << receiveDat << "\"\033[0m" << std::endl;
+        std::stringstream receiveDatSS(receiveDat);
+        int commandLen = 0, tmp, digitLen;
+        receiveDatSS >> commandLen;
+        tmp = commandLen;
+        for (digitLen = 0; tmp != 0; digitLen++) tmp /= 10;
+        strncpy(command, receiveDat + digitLen, commandLen);
+        cmd = std::string(command);
+        std::cout << "BackEnd: \033[;36m<< Handling Operation: \033[35m\"" << cmd << "\"\033[0m" << std::endl;
+        std::string resultStr = process(cmd);
+        while (resultStr[resultStr.length() - 1] == '\n'
+               || resultStr[resultStr.length() - 1] == '\r'
+               || resultStr[resultStr.length() - 1] == ' ')
+            resultStr.pop_back();
+        std::cout << "Socket:  \033[;36m>> Operation Result: \033[35m\"" << resultStr << "\"\033[0m" << std::endl;
+        std::stringstream resultSS("");
+        resultSS << std::setw(6) << std::setfill('0') << resultStr.length();
+        resultSS << resultStr;
+        strcpy(sendResult, resultSS.str().c_str());
+        send(sClient, sendResult, strlen(sendResult), 0);
+        if (resultStr == "bye")flag = false;
     }
-    return 0;
 }
--- FrontEnd/BackEnd/src/Administrator.h
+++ FrontEnd/BackEnd/src/Administrator.h
@@ -5,6 +5,18 @@
 #ifndef TICKETSYSTEM_AUTOMATA_ADMINISTRATOR_H
 #define TICKETSYSTEM_AUTOMATA_ADMINISTRATOR_H
 
+// Socket
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+// std
+#include <sstream>
+#include <iomanip>      // setw setfill
+#include <functional>   // std::ref
+#include <chrono>       // std::chrono::seconds(0)
+
+// BackEnd
 #include "UserManager.h"
 #include "TrainManager.h"
 #include "OrderManager.h"
@@ -22,22 +34,20 @@ class Administrator {
      *
      */
 private:
-    Parser       *Ptilopsis = nullptr;
-    UserManager  *Saria     = nullptr;
-    TrainManager *Silence   = nullptr;
-    OrderManager *Ifrit     = nullptr;
+    Parser *Ptilopsis;
+    UserManager *Saria;
+    TrainManager *Silence;
+    OrderManager *Ifrit;
     
     using path_t = const std::string;
     
-    path_t UserIndexPath    {"UserIndex.dat"};
-    path_t UserStoragePath  {"UserStorage.dat"};
-    path_t TrainIndexPath   {"TrainIndex.dat"};
+    path_t UserIndexPath {"UserIndex.dat"};
+    path_t UserStoragePath {"UserStorage.dat"};
+    path_t TrainIndexPath {"TrainIndex.dat"};
     path_t TrainStoragePath {"TrainStorage.dat"};
     path_t TrainStationPath {"TrainStation.dat"};
-    path_t OrderIndexPath   {"OrderIndex.dat"};
+    path_t OrderIndexPath {"OrderIndex.dat"};
     path_t OrderPendingPath {"OrderPending.dat"};
-    
-    void initialize();
     
     void clean();
     
@@ -48,7 +58,7 @@ public:
     
     ~Administrator();
     
-    int runProgramme();
+    void runProgramme();
 };
 
 #endif //TICKETSYSTEM_AUTOMATA_ADMINISTRATOR_H
